ae76223a153272e68111cf1c32112f7a0bd85a41
sweetdaniela23 <90408575+sweetdaniela23@users.noreply.github.com>
2022-02-16 10:27:35 -0700

    Add project files.

---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,61 @@
+﻿using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-blah")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //dynamically creates the page link for us
+        private IUrlHelperFactory uhf;
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+
+        public ViewContext vc { get; set; }
+        //different than view context
+        public PageInfo PageBlah { get; set; }
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false; /*added*/
+        public string PageClass { get; set; } /*added*/
+        public string PageClassNormal { get; set; } /*added*/
+        public string PageClassSelected { get; set; } /*added*/
+        //override method 
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageBlah.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled) /*added*/
+                { /*added*/
+                    tb.AddCssClass(PageClass); /*added*/
+                    tb.AddCssClass(i == PageBlah.CurrentPage /*added*/
+                    ? PageClassSelected : PageClassNormal); /*added*/
+                }
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
+
+        

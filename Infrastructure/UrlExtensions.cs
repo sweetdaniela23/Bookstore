de9b58f50697370ddac927b6e45eff8875fb9688
sweetdaniela23 <90408575+sweetdaniela23@users.noreply.github.com>
2022-02-24 10:24:40 -0700

    change in urlextensions to make it be able to run

--- Infrastructure/UrlExtensions.cs
+++ Infrastructure/UrlExtensions.cs
@@ -9,6 +9,7 @@ namespace Bookstore.Infrastructure
     public static class UrlExtensions
     {
         public static string PathAndQuery(this HttpRequest request) =>
-            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.PathAndQuery().ToString();
+            //request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.PathAndQuery().ToString();
+            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
     }
 }

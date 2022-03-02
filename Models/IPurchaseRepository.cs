ea7fb479ef4e9d3ad8390c6a17a897b074eabe11
sweetdaniela23 <90408575+sweetdaniela23@users.noreply.github.com>
2022-03-02 15:16:09 -0700

    mission9 everything works

---
+++ Models/IPurchaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public interface IPurchaseRepository
+    {
+        IQueryable<Purchase> Purchases { get; }
+        void SavePurchase(Purchase purchase);
+    }
+}
--- Pages/Select.cshtml.cs
+++ Pages/Select.cshtml.cs
@@ -13,9 +13,10 @@ namespace Bookstore.Pages
     {
         private IBookstoreRepository repo { get; set; }
 
-        public SelectModel(IBookstoreRepository temp)
+        public SelectModel(IBookstoreRepository temp, Basket b)
         {
             repo = temp;
+            basket = b;
         }
 
         public Basket basket { get; set; }
@@ -26,15 +27,17 @@ namespace Bookstore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
         }
 
         public IActionResult OnPost(int bookId, string returnUrl)
         {
             Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
-            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
             basket.AddItem(b, 1);
-            HttpContext.Session.SetJson("basket", basket);
+            return RedirectToPage(new { ReturnUrl = returnUrl });
+        }
+        public IActionResult OnPostRemove(int bookId, string returnUrl)
+        {
+            basket.RemoveItem(basket.Items.First(x => x.Book.BookId == bookId).Book);
             return RedirectToPage(new { ReturnUrl = returnUrl });
         }
     }
